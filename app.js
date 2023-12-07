f9b6bfba773b86a359eb27056a88ebdf84aabf87
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-07 13:43:30 -0500

    DB operations on SENSORS DATA -2- Logic to update sensors data every certain time done

--- app.js
+++ app.js
@@ -29,35 +29,9 @@ const start = async () => {
     //   .then((data) => console.log(data))
     //   .catch((err) => console.error('[Error]: On fetching the manual irriagation state'))
 
-
-    // const database = admin.database();
-    // const dataRef = database.ref('/DATA');
-
-    // dataRef.orderByKey().on('value', (snapshot) => {
-    //   let dataRes = {};
-    //   let lastFilter = 1;
-    //   if (lastFilter == null){
-    //     dataRes = snapshot.val();
-    //   }
-    //   else {
-    //      //json to data
-    //     mH = Object.values(snapshot.val().meanHumidity)
-    //     mL = Object.values(snapshot.val().meanLuxP)
-    //     mT = Object.values(snapshot.val().meanTemperature)
-      
-    //     lastH = mH[mH.length -1]
-    //     lastL = mL[mL.length -1]
-    //     lastT = mT[mT.length -1]
-
-    //     dataRes = {
-    //       meanHumidity: lastH,
-    //       meanLuxP: lastL,
-    //       meanTemperature: lastT
-    //     }
-    //   console.log(dataRes);
-
-    //   }
-    // });
+    const {get:getData} = require('./serverRequests/data')
+    let sensorsData = await getData(null);
+    console.log(Object.keys(sensorsData.meanHumidity).length);
 
   } catch (err) {
     console.error('No connection established to the database');
@@ -67,7 +41,7 @@ const start = async () => {
 
 start()
 
-// 
+// imports
 const {get:getRiegoManual, update:updateRiegoManual} = require('./serverRequests/riego')
 const {get:getRiegoAuto} = require('./serverRequests/riegoAuto')
 
@@ -87,51 +61,66 @@ serialport.pipe(parser);
 
 // Read the data from the serial port 
 
+let startTime = performance.now()
+let endTime;
+
 parser.on("data", (line) => {
-  console.log(line)
+  // console.log(line)
+
+  endTime = performance.now()
+  // console.log(endTime-startTime);
+
+  if ((endTime - startTime) > 20000 ) {
+    startTime = performance.now();
+    console.log('uploading....');
+  }
+  
+
+  // const {get:getData} = require('./serverRequests/data')
+  // let sensorsData = await getData(null);
   // serialport.write("0");
 });
 
 
-// Write the data to the serial port (to open or close the grifo)
-let openauto = "0"
-let openmanual = "0"
+// // Write the data to the serial port (to open or close the grifo)
+// let openauto = "0"
+// let openmanual = "0"
 
-setInterval(async () => {
+// setInterval(async () => {
 
-  // --- check on manual irrigation ---
+//   // --- check on manual irrigation ---
 
-  let openmanualNew = await getRiegoManual() === true ? '1': '0';
+//   let openmanualNew = await getRiegoManual() === true ? '1': '0';
 
-  openmanualNew === '1' ? updateRiegoManual(false) : void 0; // si es true, que la vuelva a cerrar
+//   openmanualNew === '1' ? updateRiegoManual(false) : void 0; // si es true, que la vuelva a cerrar
 
-  // openmanual = openmanual=== "1" ? "0" : "1"
+//   // openmanual = openmanual=== "1" ? "0" : "1"
 
-  // --- check on automated  irrigation ---
+//   // --- check on automated  irrigation ---
 
-  let openautoNew = await getRiegoAuto() === true ? '1': '0';
+//   let openautoNew = await getRiegoAuto() === true ? '1': '0';
 
-  // -- reading last register data ---
+//   // -- reading last register data ---
 
-  let sensorsData = await getData(null);
-  console.log(sensorsData);
+//   let sensorsData = await getData(null);
+//   console.log(sensorsData);
 
-  // --- writing on serial port --- 
+//   // --- writing on serial port --- 
 
-  // just write on serial port when the value changes on the DB, avoiding 'delays' issues on Arduino due to sending to much data
-  if ((openauto != openautoNew) || (openmanual != openmanualNew)) {
-    // console.log('changes');
+//   // just write on serial port when the value changes on the DB, avoiding 'delays' issues on Arduino due to sending to much data
+//   if ((openauto != openautoNew) || (openmanual != openmanualNew)) {
+//     // console.log('changes');
 
-    //updating, then writing new values
+//     //updating, then writing new values
 
-    openmanual = openmanualNew;
-    openauto = openautoNew;
+//     openmanual = openmanualNew;
+//     openauto = openautoNew;
 
-    serialport.write(openmanual);
-    serialport.write(openauto);
-  }
+//     serialport.write(openmanual);
+//     serialport.write(openauto);
+//   }
 
   
   
-}, 2000);
+// }, 2000);
 
